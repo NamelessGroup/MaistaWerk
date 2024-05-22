ef77b0a0ef281cfe8d42a439e5e587d9647d0455
Alexander Vogt <alexander_vogt@outlook.de>
2024-05-22 16:55:22 +0200

    better sidebar + icon

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        link: '#6a76de',
+      }
+    },
   },
   plugins: [],
 }
