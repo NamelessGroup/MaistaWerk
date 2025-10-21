60eac9fa6d1103788a6d9d88acbbd67b7c62e84b
Alexander Vogt <alexander_vogt@outlook.de>
2025-10-21 09:42:09 +0200

    adapt sidebar position

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="absolute text-white top-0 left-0 right-0 bottom-0 max-h-screen min-h-screen overflow-hidden flex bg-primary-950">
-    <RouterView class="h-full flex-1 overflow-hidden p-2" />
-    <SideBarComponent />
+  <div class="absolute text-white top-0 left-0 right-0 bottom-0 max-h-screen min-h-screen overflow-hidden grid bg-primary-950 grid-rows-[auto_1fr] grid-cols-1 md:grid-rows-1 md:grid-cols-[1fr_auto]" :class="{
+    'grid-rows-1': !sidebarCollapsed
+  }">
+    <RouterView class="h-full row-start-2 col-start-1 md:row-start-1 md:col-start-1 overflow-hidden p-2" :class="{
+      'hidden md:flex': !sidebarCollapsed,
+    }" />
+    <SideBarComponent class="row-start-1 col-start-1 md:row-start-1 md:col-start-2" v-model:collapsed="sidebarCollapsed" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import SideBarComponent from './components/SideBarComponent.vue';
+import { ref } from 'vue';
 
+const sidebarCollapsed = ref(window.matchMedia('(max-width: 768px)').matches);
 </script>
